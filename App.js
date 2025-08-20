b57bdcc7dca9f25a9cc207af57372ceec03f40c4
Lucas <lucas.alm.bsb@gmail.com>
2025-08-20 20:46:40 -0300

    criação da tela de cadastro

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import WelcomeScreen from "./src/views/WelcomeScreen";
+import RegisterScreen from "./src/views/RegisterScreen";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return <RegisterScreen />;
 }
---
+++ src/views/RegisterScreen.tsx
@@ -0,0 +1,113 @@
+import React from "react";
+import { View, Text, TextInput, StyleSheet, Image, TouchableOpacity } from "react-native";
+import Checkbox from "expo-checkbox";
+import { useRegisterViewModel } from "../viewmodels/RegisterViewModel";
+
+export default function RegisterScreen() {
+  const { form, handleChange, handleRegister } = useRegisterViewModel();
+
+  return (
+    <View style={styles.container}>
+      {/* LOGO + TEXTO NA MESMA LINHA */}
+      <View style={styles.logoContainer}>
+        <Image source={require("../../assets/logo.png")} style={styles.logo} />
+        <Text style={styles.title}>Recicla Fácil</Text>
+      </View>
+
+      <Text style={styles.label}>Email</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="seu@email.com.br"
+        value={form.email}
+        onChangeText={(text) => handleChange("email", text)}
+        keyboardType="email-address"
+      />
+
+      <Text style={styles.label}>Password</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="***********"
+        value={form.password}
+        secureTextEntry={!form.showPassword}
+        onChangeText={(text) => handleChange("password", text)}
+      />
+
+      <View style={styles.checkboxContainer}>
+        <Checkbox
+          value={form.showPassword}
+          onValueChange={(value) => handleChange("showPassword", value)}
+          color={form.showPassword ? "#4CAF50" : undefined}
+        />
+        <Text style={styles.checkboxLabel}>Mostrar Senha</Text>
+      </View>
+
+      <TouchableOpacity style={styles.button} onPress={handleRegister}>
+        <Text style={styles.buttonText}>Registrar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1B5E20",
+    padding: 20,
+  },
+  logoContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 30,
+  },
+  logo: {
+    width: 75,
+    height: 75,
+    marginRight: 10,
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+  label: {
+    alignSelf: "flex-start",
+    color: "#fff",
+    marginBottom: 5,
+    marginTop: 20,
+    marginLeft: 35,
+  },
+  input: {
+    width: "80%",
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    padding: 10,
+    marginBottom: 10,
+  },
+  checkboxContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    alignSelf: "flex-start",
+    marginBottom: 20,
+    marginTop: 15,
+    marginLeft: 35,
+  },
+  checkboxLabel: {
+    marginLeft: 8,
+    color: "#fff",
+  },
+  button: {
+    backgroundColor: "#fff",
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 8,
+    width: "80%",
+    alignItems: "center",
+    marginTop: 5,
+  },
+  buttonText: {
+    color: "#1B5E20",
+    fontWeight: "bold",
+  },
+});
